8f71801e4a2c364fc8a5a88baf803eb321fc0f1c
sangyeon <tallpizza@naver.com>
2024-08-03 20:11:59 +0900

    fix: remove unused file

--- entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-if [ -f "/app/kakao_theme_android/gradlew" ]; then
-  chmod +x /app/kakao_theme_android/gradlew
-else
-  echo "Error: gradlew not found in /app/kakao_theme_android"
-fi
--- run.sh
+++
@@ -1,1 +0,0 @@
-docker run -v "$(pwd)/kakao_theme_android:/app" -v "$(pwd)/apk_output:/app/build/outputs/apk" -w /app --rm android-theme-builder ./gradlew assembleDebug
